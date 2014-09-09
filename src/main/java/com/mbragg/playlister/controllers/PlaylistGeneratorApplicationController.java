df54860679727147248eed738b770b6f17f1640a
Michael Bragg <mikeabragg@icloud.com>
2014-09-09 10:53:01 +0100

    Application controller implementation javadoc touch

--- src/main/java/com/mbragg/playlister/controllers/PlaylistGeneratorApplicationController.java
+++ src/main/java/com/mbragg/playlister/controllers/PlaylistGeneratorApplicationController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 import java.util.concurrent.Future;
 
 /**
- * Application controller implementation - single entry point.
+ * Application controller implementation.
  *
  * @author Michael Bragg
  */
