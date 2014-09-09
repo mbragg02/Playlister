b4fa80075cefaa28423ce5febc18f43fc8f1e44d
Michael Bragg <mikeabragg@icloud.com>
2014-09-09 11:32:59 +0100

    DAO interface & implementation javadoc update / cleanup

--- src/main/java/com/mbragg/playlister/dao/DAO.java
+++ src/main/java/com/mbragg/playlister/dao/DAO.java
@@ -11,15 +11,48 @@ import java.util.List;
  * @author Michael Bragg
  */
 public interface DAO {
-    boolean genreExists(String genre);
+    /**
+     * Checks if a particular genre exists in the database.
+     *
+     * @param genreName String. The name of the genre to check.
+     * @return Boolean. Yes if it exists. No if it does not.
+     */
+    boolean genreExists(String genreName);
 
-    boolean trackExists(String filename);
+    /**
+     * Checks if a particular track exists in the database.
+     *
+     * @param fileName String. The name of the file to check.
+     * @return Boolean. Yes if it exists. No if it does not.
+     */
+    boolean trackExists(String fileName);
 
+    /**
+     * Save a Genre to the database
+     *
+     * @param genre Genre. Genre to save
+     */
     void saveGenre(Genre genre);
 
+    /**
+     * Save a Track to the database.
+     *
+     * @param track Track to save
+     */
     void saveTrack(Track track);
 
+    /**
+     * Query the database to generate a new 'Playlist'
+     *
+     * @param filename        String. Name of the file to query.
+     * @param sizeOfResult    int. Number of files in the playlist
+     * @param restrictByGenre Boolean. Whether to restrict the playlist to be all the same genre meta tag as the query file.
+     * @return A list of Tracks
+     */
     List<Track> query(String filename, int sizeOfResult, boolean restrictByGenre);
 
+    /**
+     * Delete the data in the database.
+     */
     void deleteDatabase();
 }
--- src/main/java/com/mbragg/playlister/dao/GraphDAO.java
+++ src/main/java/com/mbragg/playlister/dao/GraphDAO.java
@@ -47,31 +47,31 @@ public class GraphDAO implements DAO {
     }
 
     /**
-     * Does a genre node exists in the database.
+     * Does a genreName node exists in the database.
      *
-     * @param genre String. The genre string to test.
+     * @param genreName String. The genreName string to test.
      * @return boolean. Yes or no.
      */
     @Override
-    public boolean genreExists(String genre) {
-        return genreRepository.findByName(genre) != null;
+    public boolean genreExists(String genreName) {
+        return genreRepository.findByName(genreName) != null;
     }
 
     /**
      * Does a track node exists in the database.
      *
-     * @param filename String. The filename of a track node to test.
+     * @param fileName String. The fileName of a track node to test.
      * @return boolean. Yes or no.
      */
     @Override
-    public boolean trackExists(String filename) {
-        return trackRepository.findByFilename(filename) != null;
+    public boolean trackExists(String fileName) {
+        return trackRepository.findByFilename(fileName) != null;
     }
 
     /**
-     * Save a genre node into the database
+     * Save a genreName node into the database
      *
-     * @param genre Genre. A genre node entity.
+     * @param genre Genre. A genreName node entity.
      */
     @Override
     public void saveGenre(Genre genre) {
@@ -112,13 +112,6 @@ public class GraphDAO implements DAO {
                 }
             }
 
-            // Add similarity relationships to all other track nodes (with the same genre)
-//            for (Track nextTrack : trackRepository.findByGenreNode(genreNode)) {
-//                if (!nextTrack.equals(track)) {
-//                    double cosineSimilarity = Statistics.cosineSimilarity(track.getFeatures(), nextTrack.getFeatures());
-//                    track.relateTo(nextTrack, cosineSimilarity);
-//                }
-//            }
             trackRepository.save(track);
             newTrackTransaction.success();
         }
@@ -140,11 +133,11 @@ public class GraphDAO implements DAO {
     }
 
     /**
-     * Query the database with a filename.
+     * Query the database with a fileName.
      *
-     * @param filename        String. The filename of a track to query.
+     * @param filename        String. The fileName of a track to query.
      * @param sizeOfResult    int.
-     * @param restrictByGenre boolean. Whether to limit the scope of the search to within the same genre as the query track.
+     * @param restrictByGenre boolean. Whether to limit the scope of the search to within the same genreName as the query track.
      * @return List<Track>. List of most similar tracks.
      */
     @Override
@@ -180,12 +173,6 @@ public class GraphDAO implements DAO {
     public void deleteDatabase() {
         trackRepository.deleteAll();
         genreRepository.deleteAll();
-//        try {
-//            FileUtils.deleteRecursively(new File(dbName));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
-
 
 }
